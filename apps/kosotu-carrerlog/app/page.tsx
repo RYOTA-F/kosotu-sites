42452c0586c1789034ce9af6764e7f0bc3f8c929
RYOTA-F <ry.fj0304@gmail.com>
2023-09-24 10:26:36 +0900

    :sparkles: carrer-log setup Tailwind

--- apps/kosotu-carrerlog/app/page.tsx
+++ apps/kosotu-carrerlog/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from 'ui/components/elements/Button'
 export default function Index() {
   return (
     <>
-      <h1>kosotu-carrer-log</h1>
+      <h1 className="text-slate-500 mt-4">kosotu-carrer-log</h1>
       <Button onClick={() => alert('Carrer')}>test</Button>
     </>
   )
--- apps/kosotu-carrerlog/tailwind.config.js
+++ apps/kosotu-carrerlog/tailwind.config.js
@@ -1,11 +1,9 @@
-const ui = require('@acme/ui/tailwind')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@vercel/examples-ui/tailwind'), ui],
-  // `ui.content` includes a path to the components that are using tailwind in @acme/ui
-  content: ui.content.concat([
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@vercel/examples-ui/**/*.js',
-  ]),
+  purge: ['./app/**/*.{js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
