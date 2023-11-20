9bb0ed028f182006c75384a2f592266e752cdfc1
RYOTA-F <ry.fj0304@gmail.com>
2023-11-21 08:39:13 +0900

    :wastebasket: Mantine

--- packages/ui/src/components/blogs/Footer/Footer.test.tsx
+++ packages/ui/src/components/blogs/Footer/Footer.test.tsx
@@ -1,17 +1,12 @@
 import { describe, test, expect, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import { MantineProvider } from '@mantine/core'
 
 import { Footer, ARIA_LABEL } from '.'
 import { siteNameMock, menuListMock } from './__mocks__'
 
 describe('Footer', () => {
   beforeEach(() => {
-    render(
-      <MantineProvider>
-        <Footer siteName={siteNameMock} menuList={menuListMock} />
-      </MantineProvider>
-    )
+    render(<Footer siteName={siteNameMock} menuList={menuListMock} />)
   })
 
   test('描画される', () => {
