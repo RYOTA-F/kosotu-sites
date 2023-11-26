a28940b90387101037867920e2a309778ac5a2d5
RYOTA-F <ry.fj0304@gmail.com>
2023-11-27 00:14:27 +0900

    :wastebasket: 不要なファイルを削除

--- apps/kosotu-devlog/usecases/microCMS/blog/__mocks__/index.ts
+++
@@ -1,9 +0,0 @@
-import type { IGetBlogsResponse } from '../index'
-import { blogsMock } from './blogs'
-
-export const getBlogsMock: IGetBlogsResponse = {
-  blogs: blogsMock,
-  totalPage: 3,
-}
-
-export * from './blogs'
