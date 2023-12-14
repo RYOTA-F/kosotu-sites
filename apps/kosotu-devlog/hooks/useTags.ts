2d93b3ee010d9105f2d29aed13c5adbb5f02d5fd
RYOTA-F <ry.fj0304@gmail.com>
2023-12-14 18:21:07 +0900

    :sparkles: useTags 実装

---
+++ apps/kosotu-devlog/hooks/useTags.ts
@@ -0,0 +1,26 @@
+import { API } from 'const/microCms'
+import { MicroCmsTagUsecase } from 'usecase/microCMS/Tag/TagUsecase'
+
+/**
+ * タグ取得用カスタムフック
+ */
+export const useTags = () => {
+  /**
+   * IDを指定してタグを一件取得
+   */
+  const getTagById = async (id: string) => {
+    const { tag } = await new MicroCmsTagUsecase(
+      process.env.NEXT_PUBLIC_API_KEY || '',
+      process.env.NEXT_PUBLIC_API_ENDPOINT || '',
+      API.TAG.END_POINT
+    ).getTagById({ id })
+
+    return {
+      tag,
+    }
+  }
+
+  return {
+    getTagById,
+  }
+}
