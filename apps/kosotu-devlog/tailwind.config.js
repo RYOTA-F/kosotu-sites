4d5c9fbdfd03e73585135e62c586b7f019604330
RYOTA-F <ry.fj0304@gmail.com>
2023-12-28 17:30:24 +0900

    :sparkles: PrivacyPolicyPage 実装

--- apps/kosotu-devlog/tailwind.config.js
+++ apps/kosotu-devlog/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./app/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          text: {
+            t1: '#808080',
+            t2: '#7e8992',
+            t3: '#5e6c77',
+            t4: '#63707b',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
