09341664e44241ef9497b4edad0b47423a1cc737
RYOTA-F <ry.fj0304@gmail.com>
2023-12-28 19:42:42 +0900

    :wastebasket: IS_DEVELOPMENT 削除

--- apps/kosotu-devlog/const/env.ts
+++ apps/kosotu-devlog/const/env.ts
@@ -1,3 +1,2 @@
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || ''
 export const BASE_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT || ''
-export const IS_DEVELOPMENT = process.env.NEXT_PUBLIC_IS_MOCK_BUILD === 'true'
