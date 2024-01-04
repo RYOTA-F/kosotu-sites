dd6a7d7945a062c5f90e862007fca4e8f150eaaa
RYOTA-F <ry.fj0304@gmail.com>
2024-01-04 14:42:02 +0900

    :sparkles: HomePage 実装

--- apps/kosotu-mbalog/app/page.tsx
+++ apps/kosotu-mbalog/app/page.tsx
@@ -1,3 +1,29 @@
-export default function HomePage() {
-  return 'HomePage'
+import { API, MAX_ARTICEL_COUNT, API_KEY, API_BASE_ENDPOINT } from 'const'
+import { useArticles } from 'hook/blogs/articles/useArticles'
+import { ArticleCardList } from 'ui/components/blogs/features/articles/ArticleCardList'
+import {
+  Pagination,
+  PAGE_PATTERN,
+  TOP_PAGE_NUM,
+} from 'ui/components/blogs/common/Pagination'
+
+export default async function HomePage() {
+  const { getArticles } = useArticles({
+    apiKey: API_KEY,
+    baseEndpint: API_BASE_ENDPOINT,
+    blogEndpoint: API.BLOG.END_POINT,
+    maxPageArticleCount: MAX_ARTICEL_COUNT,
+  })
+  const { articles, totalPageCount } = await getArticles({})
+
+  return (
+    <>
+      <ArticleCardList articles={articles} />
+      <Pagination
+        totalPage={totalPageCount}
+        currentPageNumber={TOP_PAGE_NUM}
+        currentPagePattern={PAGE_PATTERN.ROOT}
+      />
+    </>
+  )
 }
---
+++ apps/kosotu-mbalog/const/microCms.ts
@@ -0,0 +1,15 @@
+export const API = {
+  BLOG: {
+    END_POINT: 'blogs',
+  },
+  CATEGORY: {
+    END_POINT: 'categories',
+  },
+  TAG: {
+    END_POINT: 'tags',
+  },
+} as const
+
+export const MAX_ARTICEL_COUNT = 10 as const
+export const FULL_ARTICEL_COUNT = 9999 as const
+export const OFFSET_ZERO = 0 as const
