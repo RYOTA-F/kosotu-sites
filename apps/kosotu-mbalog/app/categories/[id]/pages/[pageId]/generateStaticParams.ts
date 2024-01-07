8a3614d1de2352393d8680d98413bdd5eeb78ff0
RYOTA-F <ry.fj0304@gmail.com>
2024-01-07 11:54:03 +0900

    :sparkles: CategoryPage 実装

---
+++ apps/kosotu-mbalog/app/categories/[id]/pages/[pageId]/generateStaticParams.ts
@@ -0,0 +1,47 @@
+import { API, MAX_ARTICEL_COUNT, API_KEY, API_BASE_ENDPOINT } from 'const'
+import { PagePathsLogic, PAGE_TYPE } from 'logic/blogs/articles/pagePaths'
+import { PaginationLogic } from 'logic/blogs/articles/pagination'
+import { MicroCmsBlogUsecase } from 'usecase/microCMS/blog/blogUsecase'
+import { MicroCmsCategoryUsecase } from 'usecase/microCMS/category/categoryUsecase'
+
+export async function generateStaticParams() {
+  const { categories } = await new MicroCmsCategoryUsecase({
+    apiKey: API_KEY,
+    baseEndpint: API_BASE_ENDPOINT,
+    categoryEndpoint: API.CATEGORY.END_POINT,
+  }).getCategories()
+
+  // ページパス生成
+  const paths = await Promise.all(
+    categories.map(async (category) => {
+      // カテゴリ毎の記事総数
+      const { totalCount } = await new MicroCmsBlogUsecase({
+        apiKey: API_KEY,
+        baseEndpint: API_BASE_ENDPOINT,
+        blogEndpoint: API.BLOG.END_POINT,
+      }).getBlogs({
+        limit: false,
+        offset: 0,
+        maxArticleCount: 9999,
+        categoryId: category.id,
+      })
+
+      // カテゴリ毎のページ数
+      const totalPageCount = new PaginationLogic({
+        articleCount: totalCount,
+        maxPageCount: MAX_ARTICEL_COUNT,
+      }).execute()
+
+      // カテゴリ×ページ数のパス
+      const pagePaths = new PagePathsLogic({
+        totalPage: totalPageCount,
+        type: PAGE_TYPE.CATEGORY,
+        slug: category.id,
+      }).execute()
+
+      return pagePaths
+    })
+  )
+
+  return paths.flat()
+}
