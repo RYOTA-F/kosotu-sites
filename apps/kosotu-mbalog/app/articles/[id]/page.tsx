8daaf54d3adca4ed71cf7856b80b8652f74282d7
RYOTA-F <ry.fj0304@gmail.com>
2024-01-07 11:52:44 +0900

    :sparkles: ArticlePage 実装

---
+++ apps/kosotu-mbalog/app/articles/[id]/page.tsx
@@ -0,0 +1,48 @@
+import {
+  API,
+  MAX_ARTICEL_COUNT,
+  ARTICLE_DETAIL_HEADER,
+  API_KEY,
+  API_BASE_ENDPOINT,
+} from 'const'
+import { useArticles } from 'hook/blogs/articles/useArticles'
+import { ArticleDetailHeader } from 'ui/components/blogs/features/articles/ArticleDetailHeader/ArticleDetailHeader'
+import { ArticleDetailBody } from 'ui/components/blogs/features/articles/ArticleDetailBody/ArticleDetailBody'
+import { TableOfContents } from 'ui/components/blogs/common/TableOfContents'
+import { generateStaticParams } from './generateStaticParams'
+
+interface ArticlePageParams {
+  params: {
+    id: string
+  }
+}
+
+export default async function ArticlePage({
+  params: { id },
+}: ArticlePageParams) {
+  const { getArticleById } = useArticles({
+    apiKey: API_KEY,
+    baseEndpint: API_BASE_ENDPOINT,
+    blogEndpoint: API.BLOG.END_POINT,
+    maxPageArticleCount: MAX_ARTICEL_COUNT,
+  })
+  const { article, tableOfContents } = await getArticleById({ id })
+
+  return (
+    <>
+      <ArticleDetailHeader
+        article={article}
+        welcomeMessage={ARTICLE_DETAIL_HEADER.WELCOME_MESSAGE}
+        thanksMessage={ARTICLE_DETAIL_HEADER.THANKS_MESSAGE}
+      />
+      <div className="mt-8 mx-[10%]">
+        <TableOfContents tableOfContents={tableOfContents} />
+      </div>
+      <div className="mt-10">
+        <ArticleDetailBody articleBody={article.body} />
+      </div>
+    </>
+  )
+}
+
+export { generateStaticParams }
